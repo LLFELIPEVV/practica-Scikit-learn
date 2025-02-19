a9a1da5f0972a5233e175ff54c3e01e129553db5
FELIPE <felipe3133770868@gmail.com>
2025-02-19 16:01:58 -0500

    Agregar búsqueda de hiperparámetros con GridSearchCV para el modelo de regresión logística

--- metrics.py
+++ metrics.py
@@ -3,6 +3,7 @@ import pandas as pd
 import matplotlib.pyplot as plt
 
 from sklearn.linear_model import LogisticRegression
+from sklearn. model_selection import GridSearchCV
 
 df = pd.read_csv('creditcard.csv')[:80_000]
 print(df.head(3))
@@ -12,5 +13,18 @@ y = df['Class'].values
 print(f"Shapes of X={X.shape} y={y.shape}, #Fraud Cases {y.sum()}")
 
 # EL class_weight es un diccionario que permite establecer el peso que tendra cada clase del DataFrame, en este caso la clase 0 es sin fraude y la clase 1 es fraude.
-mod = LogisticRegression(class_weight={0:1, 1:2}, max_iter=1000)
+mod = LogisticRegression(class_weight={0: 1, 1: 2}, max_iter=1000)
 print(mod.fit(X, y).predict(X).sum())
+
+grid = GridSearchCV(
+    estimator=LogisticRegression(max_iter=1000),
+    param_grid={'class_weight': [{0: 1, 1: v} for v in range(1, 4)]},
+    cv=4,
+    n_jobs=-1
+)
+
+print(grid.fit(X, y))
+
+new_df = pd.DataFrame(grid.cv_results_)
+
+print(new_df)
