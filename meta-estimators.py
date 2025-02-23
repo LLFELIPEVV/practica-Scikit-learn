3e445ce774b6104f5acf648d5914cc1421033cdc
FELIPE <felipe3133770868@gmail.com>
2025-02-23 12:52:38 -0500

    Agregar implementación de Thresholder con regresión logística y optimización de hiperparámetros usando GridSearchCV para precisión, recall y exactitud.

--- meta-estimators.py
+++ meta-estimators.py
@@ -1,10 +1,17 @@
 import numpy as np
+import pandas as pd
 import matplotlib.pyplot as plt
-from sklearn.tree import DecisionTreeClassifier
-from sklearn.linear_model import LogisticRegression
-from sklearn.neighbors import KNeighborsClassifier
+
+from sklearn.pipeline import Pipeline
+from sklearn.datasets import make_blobs
 from sklearn.ensemble import VotingClassifier
+from sklearn.tree import DecisionTreeClassifier
 from sklearn.datasets import make_classification
+from sklearn.model_selection import GridSearchCV
+from sklearn.neighbors import KNeighborsClassifier
+from sklearn.linear_model import LogisticRegression
+from sklearn.metrics import precision_score, recall_score, accuracy_score, make_scorer
+from sklego.meta import Thresholder
 
 X, y = make_classification(n_samples=2000, n_features=2,
                            n_redundant=0, random_state=21,
@@ -45,3 +52,36 @@ clf3 = VotingClassifier(estimators=[('clf1', clf1), ('clf2', clf2)],
 clf3.fit(X, y)
 
 make_plots()
+
+X, y = make_blobs(1000, centers=[(0, 0), (1.5, 1.5)], cluster_std=[1, 0.5])
+plt.scatter(X[:, 0], X[:, 1], c=y, s=5)
+plt.show()
+
+m1 = Thresholder(LogisticRegression(solver='lbfgs'), threshold=0.1).fit(X, y)
+m2 = Thresholder(LogisticRegression(solver='lbfgs'), threshold=0.9).fit(X, y)
+
+plt.figure(figsize=(12, 4))
+plt.subplot(131)
+plt.scatter(X[:, 0], X[:, 1], c=y, s=5)
+plt.title("original data")
+plt.subplot(132)
+plt.scatter(X[:, 0], X[:, 1], c=m1.predict(X), s=5)
+plt.title("thresholder=0.1")
+plt.subplot(133)
+plt.scatter(X[:, 0], X[:, 1], c=m2.predict(X), s=5)
+plt.title("thresholder=0.9")
+plt.show()
+
+pipe = Pipeline([
+    ("model", Thresholder(LogisticRegression(solver='lbfgs'), threshold=0.1))
+])
+
+mod = GridSearchCV(estimator=pipe,
+                   param_grid={"model__threshold": np.linspace(0.1, 0.9, 50)},
+                   scoring={"precision": make_scorer(precision_score),
+                            "recall": make_scorer(recall_score),
+                            "accuracy": make_scorer(accuracy_score)},
+                   refit="precision",
+                   cv=5)
+
+print(mod.fit(X, y))
