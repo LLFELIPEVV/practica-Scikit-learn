fb16515642ae76424a86eb4dc552f931009f823f
FELIPE <felipe3133770868@gmail.com>
2025-02-27 15:33:38 -0500

    Agregar generación y visualización de datos aleatorios en w3_scatter_plot.py

--- w3_scatter_plot.py
+++ w3_scatter_plot.py
@@ -1,3 +1,4 @@
+import numpy as np
 import matplotlib.pyplot as plt
 
 # Un diagrama de dispersion es un diagrama en el cual cada valor del cojunto de datos se representa con un punto individual.
@@ -14,3 +15,19 @@ plt.show()
 # Explicacion del diagrama de dispersion.
 # Lo que se puede analizar del diagrama es que los dos autos mas rapidos tienen 2 años y el auto mas lento tiene 12 años.
 # Aunque esto puede ser una coincidencia ya que solo analizamos 13 datos.
+
+# Distribuciones de datos aleatorioas.
+# Se pueden realizar distribuciones de datos aleatorias para probar el funcionamiento de algun algoritmo.
+# Ejemplo: Un diagrama de dispersion con 1000 puntos.
+# Crearemos dos matrices que contengan 1000 datos aleatorios de una distribucion de datos normal.
+# La primera matriz tendra una media de 5 y una desviacion estandar de 1.
+# La segunda matriz tendra una media de 10 y una desviacion estandar de 2.
+x = np.random.normal(5, 1, 1000)
+y = np.random.normal(10, 2, 1000)
+
+plt.scatter(x, y)
+plt.show()
+
+# Explicacion del diagrama de dispersion
+# Se puede observar que los datos se concentran alrededor del 5 en el eje x y 10 en el eje y.
+# Tambien se observa que la dispersion es mayor en y que en x.
