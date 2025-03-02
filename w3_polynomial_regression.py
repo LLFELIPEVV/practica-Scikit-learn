a484d8508a96c04068d574112301b75783112f1d
FELIPE <felipe3133770868@gmail.com>
2025-03-02 18:49:11 -0500

    Agregar script para regresión polinomial con visualización y cálculo de R-Squared en w3_polynomial_regression.py

--- w3_polynomial_regression.py
+++ w3_polynomial_regression.py
@@ -39,3 +39,18 @@ print(r2_score(y, mymodel(x)))
 # Ejemplo: Predecir la velocidad de un automovil que pasa por el peaje alrededor de las 17:00.
 speed = mymodel(17)
 print(speed)
+
+# Mal ajuste
+# Ejemplo: Estos valores para los ejes x e y deberían dar como resultado un ajuste muy malo para la regresión polinomial.
+x = [89, 43, 36, 36, 95, 10, 66, 34, 38, 20,
+     26, 29, 48, 64, 6, 5, 36, 66, 72, 40]
+y = [21, 46, 3, 35, 67, 95, 53, 72, 58, 10,
+     26, 34, 90, 33, 38, 20, 56, 2, 47, 15]
+
+mymodel = np.poly1d(np.polyfit(x, y, 3))
+myline = np.linspace(2, 95, 100)
+plt.scatter(x, y)
+plt.plot(myline, mymodel(myline))
+plt.show()
+
+print(r2_score(y, mymodel(x)))
