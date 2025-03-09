8943fe8dfa67f7fb7eb6ddf999a2f4290761f4fc
FELIPE <felipe3133770868@gmail.com>
2025-03-09 18:24:22 -0500

    Agregar archivo w3_confusion_matrix.py con descripción de la matriz de confusión para evaluación de modelos de clasificación

---
+++ w3_confusion_matrix.py
@@ -0,0 +1,4 @@
+# Matriz de confusion
+# Es una tabla que se utiliza en problemas de clasificacion para evaluar donde se cometieron errores en el modelo.
+# Con esta tabla es facil ver que predicciones son erroneas.
+# Las filas representan las clases reales a las que deberian haber pertenecido los resultados, mientras que las columnas representan las predicciones que hemos realizado.
