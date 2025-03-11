e0d56d70841a0b7277172bdf90163ba8f834d59d
FELIPE <felipe3133770868@gmail.com>
2025-03-11 15:30:23 -0500

    Agregar archivo w3_hierarchical_clustering.py con documentación sobre el agrupamiento jerárquico

---
+++ w3_hierarchical_clustering.py
@@ -0,0 +1,7 @@
+# Agrupamiento jerarquico
+# La agrupacion jerarquica es un metodo de apredizaje no supervisado para agrupar puntos de datos.
+# El algoritmo crea grupos midiendo las diferencias entre los datos.
+# El aprendizaje no supervisado significa que no es necesario entrenar un modelo y no necesitamos una variable 'objetivo'.
+# Este metodo se puede utilizar con cualquier dato para visualizar e interpretar la relacion entre puntos de datos individuales.
+
+# ¿Como funciona?
