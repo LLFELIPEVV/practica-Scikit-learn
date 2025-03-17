5930ecb992371b1ca2ebf9011fc750a3c8fe67ca
FELIPE <felipe3133770868@gmail.com>
2025-03-17 15:41:01 -0500

    Agregar predicción de CO2 utilizando regresión lineal en w3_categorical_data.py

--- w3_categorical_data.py
+++ w3_categorical_data.py
@@ -1,5 +1,7 @@
 import pandas as pd
 
+from sklearn import linear_model
+
 # Datos categoricos
 # Cuando los datos tienen categorias representadas por cadenas, sera dificil usarlas para entrenar modelos de aprendizaje automatico que a menudo solo aceptan datos numericos.
 # En lugar de ignorar los datos categoricos, se pueden transformar.
@@ -14,3 +16,15 @@ print(cars.head())
 # Para cada columna, los valores seran 1 o 0, donde 1 representa la inclusion del grupo y 0 la exclusion. Esta transformacion se denomina codificacion one-hot.
 ohe_cars = pd.get_dummies(cars[["Car"]])
 print(ohe_cars.head())
+
+# Predecir el CO2
+X = pd.concat([cars[["Volume", "Weight"]], ohe_cars], axis=1)
+y = cars["CO2"]
+
+regr = linear_model.LinearRegression()
+regr.fit(X, y)
+
+predictedCO2 = regr.predict(
+    [[2300, 1300, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0]])
+
+print(predictedCO2)
