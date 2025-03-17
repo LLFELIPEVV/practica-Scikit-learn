ba6c65a102c7b6b2850323a59656cb212deb7099
FELIPE <felipe3133770868@gmail.com>
2025-03-17 15:36:34 -0500

    Agregar codificación One Hot para la columna 'Car' en w3_categorical_data.py

--- w3_categorical_data.py
+++ w3_categorical_data.py
@@ -7,3 +7,10 @@ cars = pd.read_csv("data.csv")
 print(cars.head())
 
 # En regresion multiple tratamos de predecir el CO2 emitido por un vehiculo en funcion del motor y el peso, pero excluimos informacion sobre la marca y el modelo del auto.
+
+# Codificacion One Hot
+# No se puede utilizar la columna Coche ni la columna Modelo en nuestros datos, ya que no son numericas. No se puede determinar una relacion lineal entre una variable categorica y una variable numerica.
+# Para solucionar este problema, necesitamos una representacion numerica de la variable categorica. Una forma de lograrlo es tener una columna que represente cada grupo de la categoria.
+# Para cada columna, los valores seran 1 o 0, donde 1 representa la inclusion del grupo y 0 la exclusion. Esta transformacion se denomina codificacion one-hot.
+ohe_cars = pd.get_dummies(cars[["Car"]])
+print(ohe_cars.head())
