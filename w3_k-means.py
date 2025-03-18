14b23084375056b860bbded24b3160e84c467321
FELIPE <felipe3133770868@gmail.com>
2025-03-18 14:33:57 -0500

    Agregar archivo w3_k-means.py con descripción del algoritmo K-means para agrupamiento de datos

---
+++ w3_k-means.py
@@ -0,0 +1,3 @@
+# K-medias
+# K-means es un metodo de aprendizaje no supervisado para agrupar puntos de datos.
+# El algoritmo divide iterativamente los puntos de datos en K grupos, minimizando la varianza de cada uno.
