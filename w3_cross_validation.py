012618e58b0c14e62e54f39743b33e2730c48e92
FELIPE <felipe3133770868@gmail.com>
2025-03-23 17:35:05 -0500

    Agregar archivo w3_cross_validation.py con explicación sobre la validación cruzada y su importancia en el ajuste de modelos.

---
+++ w3_cross_validation.py
@@ -0,0 +1,2 @@
+# Validacion Cruzada
+# Al ajustar los modelos se busca mejorar el rendimiento general con datos no analizados. Ajustar los hiperparametros puede mejorar considerablemente el rendimiento de los conjuntos de prueba. Sin embargo optimizar los parametros para el conjunto de prueba puede provocar fugas de informacion, lo que empeora el rendimiento del modelo con datos no analizados. Para corregir esto, se puede realizar una validacion cruzada.
