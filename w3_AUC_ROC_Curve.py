457aff401ae7a6e5f83ec82c66dfd73ba9f1aaac
FELIPE <felipe3133770868@gmail.com>
2025-03-26 14:51:06 -0500

    Agregar implementación de la curva AUC-ROC en w3_AUC_ROC_Curve.py para evaluar la precisión del modelo en clasificación binaria.

---
+++ w3_AUC_ROC_Curve.py
@@ -0,0 +1,5 @@
+# Curva AUC-ROC
+# En la clasificacion existen, muchas metricas de evaluacion diferentes. La mas popular es la presicion, que mide la frecuencia con la que el modelo acierta.
+# En algunos casos, se podria considerar el uso de otra metrica de evaluacion.
+# Una de esas metricas es el AUC (area bajo la curva ROC). Esta curva representa la tasa de verdaderos positivos frente a la de falsos positivos en diferentes umbrales de clasificacion. Los umbrales son diferentes puntos de corte de probabilidad que separan las dos clases en la clasificacion binaria.
+# Esta utiliza la probabilidad para indicar la presicion con la que un modelo separa clases.
