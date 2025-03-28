21294beda45f98b34fc192acc8b2b18b30b8fa27
FELIPE <felipe3133770868@gmail.com>
2025-03-28 14:29:16 -0500

    Agregar implementación del modelo KNN en w3_KNN.py y visualización de predicciones para nuevos puntos.

--- w3_KNN.py
+++ w3_KNN.py
@@ -1,5 +1,7 @@
 import matplotlib.pyplot as plt
 
+from sklearn.neighbors import KNeighborsClassifier
+
 # KNN es un algoritmo de aprendizaje automatico supervisado.
 # K es el numero de vecinos mas cercanos que se utilizaran para la clasificacion.
 # Para la clasificacion se utiliza una mayoria de votos para determinar la clase a la que debe pertenecer una nueva observacion.
@@ -9,4 +11,19 @@ y = [21, 19, 24, 17, 16, 25, 24, 22, 21, 21]
 classes = [0, 0, 1, 0, 0, 1, 1, 0, 1, 1]
 
 plt.scatter(x, y, c=classes)
+plt.show()
+
+# Ahora se creara el modelo con el algoritmo KNN con K=1
+data = list(zip(x, y))
+knn = KNeighborsClassifier(n_neighbors=1)
+knn.fit(data, classes)
+
+new_x = 8
+new_y = 21
+new_point = [(new_x, new_y)]
+
+prediction = knn.predict(new_point)
+
+plt.scatter(x + [new_x], y + [new_y], c=classes + [prediction[0]])
+plt.text(x=new_x-1.7, y=new_y-0.7, s=f"new point, class: {prediction[0]}")
 plt.show()
