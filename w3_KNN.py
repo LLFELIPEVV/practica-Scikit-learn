75f72536c8d38709a251c82b692dccb7b64f2200
FELIPE <felipe3133770868@gmail.com>
2025-03-28 14:31:07 -0500

    Agregar implementación de KNN con un valor mayor para K en w3_KNN.py y visualización de predicciones.

--- w3_KNN.py
+++ w3_KNN.py
@@ -27,3 +27,13 @@ prediction = knn.predict(new_point)
 plt.scatter(x + [new_x], y + [new_y], c=classes + [prediction[0]])
 plt.text(x=new_x-1.7, y=new_y-0.7, s=f"new point, class: {prediction[0]}")
 plt.show()
+
+# Ahora con un valor mayor para K
+knn = KNeighborsClassifier(n_neighbors=5)
+knn.fit(data, classes)
+
+prediction = knn.predict(new_point)
+
+plt.scatter(x + [new_x], y + [new_y], c=classes + [prediction[0]])
+plt.text(x=new_x-1.7, y=new_y-0.7, s=f"new point, class: {prediction[0]}")
+plt.show()
