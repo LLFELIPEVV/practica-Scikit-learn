077de6f44476c61fd124c00e14ee77bd7ea456ff
FELIPE <felipe3133770868@gmail.com>
2025-03-29 18:43:50 -0500

    Agregar archivo w3_scipy_home.py con ejemplos de uso de la biblioteca Scipy y constantes científicas.

---
+++ w3_scipy_home.py
@@ -0,0 +1,5 @@
+from scipy import constants
+# Scipy es una biblioteca de calculo cientifico que utiliza Numpy como base.
+# Scipy significa Python cientifico.
+# Ejemplo: ¿Cuantos metros cubicos hay en un litro?
+print(constants.liter)
