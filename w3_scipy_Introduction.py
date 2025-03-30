609bac59f556cd901e8a00a49eb069d1cf6346a4
FELIPE <felipe3133770868@gmail.com>
2025-03-30 17:57:36 -0500

    Agregar archivo w3_scipy_Introduction.py con introducción a Scipy y sus ventajas sobre Numpy.

---
+++ w3_scipy_Introduction.py
@@ -0,0 +1,6 @@
+# Scipy ofrece mas funciones de utilidad para optimizacion, estadisticas y procesamiento de señales.
+# Al igual que Numpy, Scipy es de codigo abierto, por lo que podemos usarlo libremente.
+
+# ¿Por que usar Scipy?
+# Si Scipy usa Numpy, ¿Por que no usar Numpy sin mas?
+# Scipy ha optimizado y añadido funciones que se usan frecuentemente en Numpy y la ciencia de datos.
