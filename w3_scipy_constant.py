d990ad72268d64797618417a55d035f5fa78af64
FELIPE <felipe3133770868@gmail.com>
2025-04-05 18:20:10 -0500

    Agregar archivo w3_scipy_constant.py con ejemplos de uso de constantes científicas de Scipy.

---
+++ w3_scipy_constant.py
@@ -0,0 +1,5 @@
+from scipy import constants
+
+# Scipy esta enfocado en implementaciones cientificas, por lo que proporciona muchas constantes cientificas integradas.
+# Ejemplo: usar la constante de Pi.
+print(constants.pi)
