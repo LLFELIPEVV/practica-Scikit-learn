72ad09a27cd29a082aca7323793216221454853c
FELIPE <felipe3133770868@gmail.com>
2025-04-06 15:02:16 -0500

    Agregar ejemplos de uso de unidades constantes en w3_scipy_constant.py

--- w3_scipy_constant.py
+++ w3_scipy_constant.py
@@ -3,3 +3,7 @@ from scipy import constants
 # Scipy esta enfocado en implementaciones cientificas, por lo que proporciona muchas constantes cientificas integradas.
 # Ejemplo: usar la constante de Pi.
 print(constants.pi)
+
+# Unidades constantes
+# Se puede ver una lista de todas las unidades bajo el modulo de constantes usando la funcion dir().
+print(dir(constants))
