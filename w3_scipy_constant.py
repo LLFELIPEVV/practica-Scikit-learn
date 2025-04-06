cc595780bc918bc31644b35a929bdd91dc266676
FELIPE <felipe3133770868@gmail.com>
2025-04-06 15:04:08 -0500

    Agregar categorías de unidades en w3_scipy_constant.py

--- w3_scipy_constant.py
+++ w3_scipy_constant.py
@@ -7,3 +7,19 @@ print(constants.pi)
 # Unidades constantes
 # Se puede ver una lista de todas las unidades bajo el modulo de constantes usando la funcion dir().
 print(dir(constants))
+
+# Categorias de unidades
+# Las unidades se clasifican en estas categorias:
+# Metric
+# Binary
+# Mass
+# Angle
+# Time
+# Length
+# Pressure
+# Volume
+# Speed
+# Temperature
+# Energy
+# Power
+# Force
